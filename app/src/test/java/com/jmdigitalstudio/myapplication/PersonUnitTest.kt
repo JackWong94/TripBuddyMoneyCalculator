12f6b8ddb5673212d90ba6c4747ecf4144605bb5
JackWong94 <w.yuheng94@gmail.com>
2024-02-25 20:51:33 +1300

    Create unit test for Person class

---
+++ app/src/test/java/com/jmdigitalstudio/myapplication/PersonUnitTest.kt
@@ -0,0 +1,47 @@
+package com.jmdigitalstudio.myapplication
+
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class PersonUnitTest {
+
+    //Person Class Unit Test
+    @Test
+    fun `addPerson should add a new person`() {
+        // Arrange
+        val name = "Jack"
+
+        // Act
+        PersonManager.addPerson(name)
+
+        // Assert
+        assertEquals(1, PersonManager.people.size)
+        assertEquals(name, PersonManager.people[0].name)
+    }
+
+    @Test
+    fun `getPersonByName should return the correct person`() {
+        // Arrange
+        val name = "Jack"
+        val person = Person(name, 0.0, 0.0)
+        PersonManager.people.add(person)
+
+        // Act
+        val result = PersonManager.getPersonByName(name)
+
+        // Assert
+        assertEquals(person, result)
+    }
+
+    @Before
+    fun setUp() {
+        PersonManager.people.clear()
+    }
+
+}
