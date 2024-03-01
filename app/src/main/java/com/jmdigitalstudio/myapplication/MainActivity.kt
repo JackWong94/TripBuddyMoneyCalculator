295ec09eef0d766f26ed5c0ea5b09d8ba5940872
JackWong94 <w.yuheng94@gmail.com>
2024-03-01 22:41:14 +1300

    Add Title, Calculate Button UI

--- app/src/main/java/com/jmdigitalstudio/myapplication/MainActivity.kt
+++ app/src/main/java/com/jmdigitalstudio/myapplication/MainActivity.kt
@@ -30,11 +30,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
@@ -54,16 +56,45 @@ class MainActivity : ComponentActivity() {
                     CalculatingManager.calculationSetup()
                     //CalculatingManager.summary()
                     //CalculatingManager.findPayoffSolution()
-                    ItemDetailsList(ItemManager.items)
+                    TripBuddyMoneyCalculatorApp()
                 }
             }
         }
     }
 }
 @Composable
+private fun TripBuddyMoneyCalculatorApp() {
+    Column (
+        modifier = Modifier
+            .padding(dimensionResource(id = R.dimen.padding_medium)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ){
+        TitleDisplay()
+        CurrentTripTitle()
+        ItemDetailsList(ItemManager.items)
+        ReadyToCalculateButton()
+    }
+}
+@Composable
+fun ReadyToCalculateButton() {
+    Column(
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = { /*TODO*/ },
+        ) {
+            Text(text = "END OF TRIP --> CLICK TO CALCULATE !")
+        }
+    }
+}
+@Composable
 fun ItemDetailsList(itemList: List<Item>) {
     Column (
-        modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_small)), // Add padding to the whole Column
+        modifier = Modifier, // Add padding to the whole Column
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
     ){
         LazyColumn (
             verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small)),
@@ -72,14 +103,25 @@ fun ItemDetailsList(itemList: List<Item>) {
                 ItemDetailsCard(item = it)
             }
         }
+        Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.padding_medium)))
         FloatingActionButton(
             onClick = { /*TODO*/ },
+            Modifier.size(30.dp)
         ) {
             Icon(Icons.Filled.Add, null )
         }
     }
 }
-
+@Composable
+fun CurrentTripTitle() {
+    CustomText(
+        text = "Current Trip: Waipu Trip",
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Bold
+    )
+    CustomText(text = "Click here to change to other trips", underline = true, color = Color.Blue)
+    Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.padding_medium)))
+}
 @Composable
 fun ItemDetailsCard(item: Item, modifier: Modifier = Modifier) {
     Card (
@@ -113,7 +155,7 @@ fun ItemDetailsCard(item: Item, modifier: Modifier = Modifier) {
                     .padding(dimensionResource(R.dimen.padding_small))
             ) {
                 Column {
-                    Row (Modifier.padding(dimensionResource(R.dimen.padding_small))){
+                    Row {
                         CustomText(text = "Owed by: ")
                         FloatingActionButton(
                             onClick = { /*TODO*/ },
@@ -123,6 +165,7 @@ fun ItemDetailsCard(item: Item, modifier: Modifier = Modifier) {
                             Icon(Icons.Filled.Add, null )
                         }
                     }
+                    Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.padding_extra_small)))
                     HorizontalSrollingView(item = item)
                 }
             }
@@ -135,7 +178,8 @@ fun HorizontalSrollingView(item: Item) {
     LazyRow (
         modifier = Modifier
             .background(Color.White, shape = RoundedCornerShape(16.dp))
-            .padding(dimensionResource(R.dimen.padding_small)),
+            .padding(dimensionResource(R.dimen.padding_small))
+            .height(30.dp),
         horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small)),
     ) {
         val oweByList = item.owedBy.zip(item.owedAmount) { person, amount ->
@@ -157,16 +201,18 @@ fun HorizontalSrollingView(item: Item) {
 }
 
 @Composable
-fun Greeting(content: String, modifier: Modifier = Modifier) {
+fun TitleDisplay(modifier: Modifier = Modifier) {
     CustomText(
-        text = "Welcome To Trip Buddy Money Calculator\n $content",
+        text = "Welcome To Trip Buddy Money Calculator"
     )
+    Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.padding_medium)))
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TripBuddyMoneyCalculatorPreview() {
     TripBuddyMoneyCalculatorTheme {
+
         // Reset ItemManager and PersonManager before adding a new item
         ItemManager.items.clear()
         PersonManager.people.clear()
@@ -176,15 +222,7 @@ fun GreetingPreview() {
         PersonManager.addPerson("Anna")
         // Add a new item to ItemManager
         ItemManager.addOwingItem("Oyster", 20.0, PersonManager.getPersonByName("Jack"), PersonManager.people)
-
-        // Use remember to ensure itemData is recomposed properly
-        val itemData = remember { ItemManager.items.firstOrNull() }
-        if (itemData != null) {
-            ItemDetailsCard(item = itemData)
-        } else {
-            // Handle the case when there are no items
-            CustomText(text = "No items available")
-        }
+        TripBuddyMoneyCalculatorApp()
     }
 }
 
@@ -193,14 +231,21 @@ fun CustomText(
     text: String,
     fontSize: TextUnit = 10.sp,
     color: Color = Color.Black,
-    fontWeight: FontWeight? = null
+    fontWeight: FontWeight? = null,
+    fontStyle: FontStyle? = null,
+    underline: Boolean = false, // Added underline parameter
 ) {
+    val textDecoration = if (underline) TextDecoration.Underline else TextDecoration.None
+
     Text(
+
         text = text,
         fontSize = fontSize,
         color = color,
         fontWeight = fontWeight,
-        lineHeight = 12.sp
+        lineHeight = 12.sp,
+        fontStyle = fontStyle,
+        textDecoration = textDecoration,
     )
     Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.padding_small)))
 }
