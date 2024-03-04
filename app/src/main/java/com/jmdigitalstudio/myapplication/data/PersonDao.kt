a036e6e13eb2dc2a06b9b48cf57c0fe93d34227a
JackWong94 <w.yuheng94@gmail.com>
2024-03-04 20:57:36 +1300

    ROOM implementation - Create Data Access Object DAO for items and people

---
+++ app/src/main/java/com/jmdigitalstudio/myapplication/data/PersonDao.kt
@@ -0,0 +1,24 @@
+package com.jmdigitalstudio.myapplication.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface PersonDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(person: Person)
+    @Update
+    suspend fun update(person: Person)
+    @Delete
+    suspend fun delete(person: Person)
+    @Query("SELECT * from people WHERE id = :id")
+    fun getPerson(id: Int): Flow<Person>
+
+    @Query("SELECT * from people ORDER BY name ASC")
+    fun getAllPeople(): Flow<List<Person>>
+}
